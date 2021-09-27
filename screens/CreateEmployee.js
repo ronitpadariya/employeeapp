2e7c05050d86a8104818dba21cf1c4418cb09a07
ronit <RonitEcosmob>
2021-09-27 14:50:37 +0530

    Uploading images to Cloudinary

--- screens/CreateEmployee.js
+++ screens/CreateEmployee.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Modal } from 'react-native';
+import { StyleSheet, Text, View, Modal, Alert } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
@@ -13,6 +13,64 @@ const CreateEmployee = ()=>{
     const [salary, setSalary] = React.useState("")
     const [picture, setPicture] = React.useState("")
     const [modal, setModal] = React.useState(false)
+
+    const pickFromGallery = async ()=>{
+        const {granted} = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+        if(granted){
+            let data = await ImagePicker.launchImageLibraryAsync({
+                mediTypes:ImagePicker.MediaTypeOptions.Images,
+                allowEditing:true,
+                aspect:[1,1],
+                quality:0.5
+            })
+            console.log(data);
+            if(!data.cancelled){
+                let newFile = { uri:data.uri, 
+                type:`test/${data.uri.split(".")[1]}`, 
+                name:`test.${data.uri.split(".")[1]}`}
+                handleUpload(newFile)
+            }
+        } else {
+            Alert.alert("You need to give up permissionto work")
+        }
+    }
+
+    const pickFromCamera = async ()=>{
+        const {granted} = await Permissions.askAsync(Permissions.CAMERA)
+        if(granted){
+            let data = await ImagePicker.launchCameraAsync({
+                mediTypes:ImagePicker.MediaTypeOptions.Images,
+                allowEditing:true,
+                aspect:[1,1],
+                quality:0.5
+            })
+            console.log(data);
+            if(!data.cancelled){
+                let newFile = { uri:data.uri, 
+                type:`test/${data.uri.split(".")[1]}`, 
+                name:`test.${data.uri.split(".")[1]}`}
+                handleUpload(newFile)
+            }
+        } else {
+            Alert.alert("You need to give up permissionto work")
+        }
+    }
+
+    const handleUpload = (image)=> {
+        const data = new FormData()
+        data.append('file', image)
+        data.append('upload_preset', 'employeeApp')
+        data.append('cloud_name', 'dzflnh0xb')
+
+        fetch("https://api.cloudinary.com/v1_1/dzflnh0xb/image/upload",{
+            method:"post",
+            body:data
+        }).then(res=>res.json())
+        .then(data=>{
+            setPicture(data.url)
+            setModal(false)
+        })
+    }
 
     return(
         <View style={styles.root}>
@@ -57,7 +115,7 @@ const CreateEmployee = ()=>{
 
             <Button 
                 style={styles.inputStyle}
-                icon="upload" 
+                icon={picture==""?"upload":"check"}
                 mode="contained"
                 theme={theme}
                 onPress={() => setModal(true)}>
@@ -86,13 +144,13 @@ const CreateEmployee = ()=>{
                         <Button icon="camera" 
                             theme={theme}
                             mode="contained"
-                            onPress={() => console.log("preses")}>
+                            onPress={() => pickFromCamera()}>
                                 camera
                         </Button>
                         <Button icon="image-area" 
                             mode="contained"
                             theme={theme} 
-                            onPress={() => console.log("preses")}>
+                            onPress={() => pickFromGallery()}>
                                 gallery
                         </Button>
                     </View>
